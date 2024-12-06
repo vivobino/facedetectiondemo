83c443fcfb71c0ea3473c7e9204b59be9a30a752
vivobino <vivobino94@gmail.com>
2024-12-06 15:16:03 +0300

    first commit for this project

---
+++ code_1.py
@@ -0,0 +1,10 @@
+import numpy as np
+import cv2
+img = cv2.imread(r"C:\Users\Admin\Desktop\computervision\Trained data\WIN_20241206_14_05_25_Pro.jpg", 1)
+
+cv2.imshow('Image', img)
+
+cv2.destroyAllWindows()
+
+
+
